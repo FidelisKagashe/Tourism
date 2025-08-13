2fd1ed75416b95d85029559d86937d3332f31a25
FidelisKagashe <fideliskagashe@gmail.com>
2025-08-13 13:47:21 +0300

    Project improvement

--- tanzania_tourism/settings.py
+++ tanzania_tourism/settings.py
@@ -9,8 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='your-secret-key-here-change-in-production')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG = False
+DEBUG = config('DEBUG', default=True, cast=bool)
+
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'tourismproject.pythonanywhere.com']
 
 # Application definition
