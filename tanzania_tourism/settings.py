6799e17b8c74702562909a683f2697a6a80e33c5
FidelisKagashe <fideliskagashe@gmail.com>
2025-08-18 20:11:58 +0300

    Improvement

--- tanzania_tourism/settings.py
+++ tanzania_tourism/settings.py
@@ -9,10 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='your-secret-key-here-change-in-production')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'tourismproject.pythonanywhere.com']
-#ALLOWED_HOSTS = ['localhost',]
+# ALLOWED_HOSTS = ['localhost',]
 # Application definition
 DJANGO_APPS = [
     'django.contrib.admin',
